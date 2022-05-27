49c83baeb17b040d5c1d3da6a00b5c18818b3b3a
Eddie Shev <eddieshev@gmail.com>
2022-05-27 18:53:59 -0400

    style(css): refactored css styles sheets

---
+++ client/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>copyright &copy;2022 UpWatch.</p>
+    </div>
+  );
+};
+
+export default Footer;
